b72608192cefb4f2ff90cb14b34b9d1b5fdf6410
bluekdev <kevinquinde98@gmail.com>
2022-12-19 23:04:28 -0500

    Fix start demo

--- controllers/users.js
+++ controllers/users.js
@@ -66,7 +66,7 @@ module.exports.login = async (req, res, next) => {
         "error",
         "El correo electrónico o la contraseña son incorrectos!"
       );
-      res.redirect("/login");
+      res.redirect("/register");
     } else {
       // CHECK CREDENTIALS
       const comparison = await bcrypt.compare(
